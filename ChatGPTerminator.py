98098326d965cec277f94e42817cae3886c2acd2
AineeJames <olsenaiden33@gmail.com>
2023-03-13 14:09:37 -0700

    propt text

--- ChatGPTerminator.py
+++ ChatGPTerminator.py
@@ -1,17 +1,38 @@
 import openai
-from rich import print, inspect
+from rich.prompt import Prompt
+from rich.console import Console
+from rich.markdown import Markdown
 import tomllib
 
-with open("config.toml", "rb") as f:
-    config = tomllib.load(f)
+if __name__ == '__main__':
 
-welcome_ascii = '''
-   _____ _____ _______                  _             _             
-  / ____|  __ \__   __|                (_)           | |            
- | |  __| |__) | | | ___ _ __ _ __ ___  _ _ __   __ _| |_ ___  _ __ 
- | | |_ |  ___/  | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | __/ _ \| '__|
- | |__| | |      | |  __/ |  | | | | | | | | | | (_| | || (_) | |   
-  \_____|_|      |_|\___|_|  |_| |_| |_|_|_| |_|\__,_|\__\___/|_|   
-'''
-print(f"[bold green]{welcome_ascii}[/bold green]")
-print(f"[dark-green]Model: {config['model']}[/dark-green]")
+    console = Console()
+
+    with open("config.toml", "rb") as f:
+        config = tomllib.load(f)
+
+    welcome_ascii = '''
+       _____ _____ _______                  _             _             
+      / ____|  __ \__   __|                (_)           | |            
+     | |  __| |__) | | | ___ _ __ _ __ ___  _ _ __   __ _| |_ ___  _ __ 
+     | | |_ |  ___/  | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | __/ _ \| '__|
+     | |__| | |      | |  __/ |  | | | | | | | | | | (_| | || (_) | |   
+      \_____|_|      |_|\___|_|  |_| |_| |_|_|_| |_|\__,_|\__\___/|_|   '''
+    console.print(f"[bold green]{welcome_ascii}[/bold green]")
+    console.print(f"[dark-green]Model: {config['model']}[/dark-green]\n")
+
+    usr_in = Prompt.ask("[bold green]Input[/bold green][bold gray]>[/bold gray]")
+    resp = '''
+    This is a test:
+    ```python
+    import os
+    print("hello world")
+    # here is a comment
+    ```
+    - a
+    - b
+    - c
+    '''
+    resp_md = Markdown(resp)
+    console.print(f"[bold green]GPTerminator[/bold green][bold gray] > [/bold gray]")
+    console.print(resp_md)
