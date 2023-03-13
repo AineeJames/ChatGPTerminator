277f5fcbaa6450a73bfe5c2bb9c096ab26c6141d
2chainzbear <jameswilcock8@gmail.com>
2023-03-13 14:11:59 -0700

    added description of format desired

--- ChatGPTerminator.py
+++ ChatGPTerminator.py
@@ -8,6 +8,15 @@ import os
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def complete_chat(prompt: str,chat_history: list[dict]) -> str:
+    ''' 
+	chat_hist is list of messages in format
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+	'''
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages= chat_history + {"role": "user", "content": prompt}
